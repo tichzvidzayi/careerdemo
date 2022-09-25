3f12f9a6509c4e69762e323d43934a2bb9efbfa6
Tichaona Zvidzayi <tzvidzie@gmail.com>
2022-09-25 11:53:10 +0200

    change

--- careerspage/src/components/HelloWorld.vue
+++ careerspage/src/components/HelloWorld.vue
@@ -11,13 +11,14 @@ export default {
     msg: String
   }
 }
+
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 h3 {
   margin: 40px 0 0;
-  
+
 }
 ul {
   list-style-type: none;
